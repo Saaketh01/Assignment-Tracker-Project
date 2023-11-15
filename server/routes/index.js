468e744169a9e47365dec79632efdeee0ab3e680
Saaketh01 <144926988+Saaketh01@users.noreply.github.com>
2023-11-15 18:13:18 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET method home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Home Page',    
   });
 });
 
-/* GET home page. */
+/* GET method home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { 
     title: 'Home',    
